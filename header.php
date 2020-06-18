8aa34f56a8eb355d3762a6075090d4c892923dd0
Mtuhiko Gusikenn <chanmitsu35@gmail.com>
2020-06-18 15:33:46 +0900

    手直ししました

--- header.php
+++ header.php
@@ -14,20 +14,13 @@
     <header>
       
       <nav class="menu">
-<<<<<<< HEAD
-=======
         
->>>>>>> master
 
         <?php wp_nav_menu( array(
             'theme_location'=>'mainmenu', 
             'container'     =>'', 
             'menu_class'    =>'',
             'items_wrap'    =>'<ul id="main-nav">%3$s</ul>'));
-<<<<<<< HEAD
-        ?>
-=======
         ?> 
->>>>>>> master
       </nav>
     </header>
