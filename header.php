545619e98a444f0e9f35aece584e1dbbd6e28ad9
MiyoshiFumiko <rhapsody.in.blue926@gmail.com>
2020-06-19 15:25:56 +0900

    readmoreの下線にマウスオーバー時のアニメーション追加

--- header.php
+++ header.php
@@ -6,17 +6,14 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" ></script>
+  
   <!-- <title>About Dog</title> -->
   <?php wp_head(); ?>
 </head>
 <body>
   <div class="container">
     <header>
-      <div class="menu-btn">
-        <i class="fa fa-bars" aria-hidden="true"></i>
-      </div>
+      
       <nav class="menu">
         
           <?php wp_nav_menu( array(
